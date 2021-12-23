d3eac8e0d60b9fb09961fff39fba9965aac7c758
Christopher Fenner <CFenner@users.noreply.github.com>
2021-12-23 21:09:20 +0100

    feat: allow album art on the left (#31)
    * allow album art on the left
    
    * Update README.md

--- MMM-Sonos.js
+++ MMM-Sonos.js
@@ -8,6 +8,7 @@
 	defaults: {
 		showStoppedRoom: true,
 		showAlbumArt: true,
+		showAlbumArtRight: true,
 		showRoomName: true,
 		animationSpeed: 1000,
 		updateInterval: 0.5, // every 0.5 minutes
@@ -78,9 +79,14 @@
 		// show song if PLAYING
 		if(state === 'PLAYING' && !isEmpty) {
 			room += this.html.song.format(
+				// show album art if 'showAlbumArt' is set
+				(this.config.showAlbumArt && !this.config.showAlbumArtRight
+					?this.html.art.format(cover)
+					:''
+				)+
 				this.html.name.format(artist, track)+
 				// show album art if 'showAlbumArt' is set
-				(this.config.showAlbumArt
+				(this.config.showAlbumArt && this.config.showAlbumArtRight
 					?this.html.art.format(cover)
 					:''
 				)
