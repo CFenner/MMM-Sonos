1a7d9abbca7b723e141e326c56f071a0d1667b1a
CFenner <christopher.fenner@me.com>
2022-01-07 09:53:18 +0100

    migrate to nunjucks usage

--- MMM-Sonos.js
+++ MMM-Sonos.js
@@ -32,99 +32,62 @@
 			this.config.apiBase + ":" + this.config.apiPort + "/" + this.config.apiEndpoint);
 	},
 	getRoomName: function(room){
-		var roomName = '';
-		var isGroup = room.members.length > 1;
-		if(isGroup){
+		var roomList = [];
+		if(room.members.length > 1){
                 	$.each(room.members, function (j, member) {
-                                var isExcluded = this.config.exclude.indexOf(member.roomName) !== -1;
-                                roomName += isExcluded?'':(member.roomName + ', ');
+				if (!this.isRoomExcluded(member.roomName))
+					roomList.push(member.roomName);
                         }.bind(this));
-                        roomName = room.replace(/, $/,"");
                 }else{
-                        roomName = room.coordinator.roomName;
-                        var isExcluded = this.config.exclude.indexOf(room) !== -1;
-                        roomName = isExcluded?'':room;
+			if (!this.isRoomExcluded(room.coordinator.roomName))
+				roomList.push(room.coordinator.roomName);
                 }
-		return roomName;
+		return roomList.join(', ');
 	},
-	render: function(data){
-		var text = '';
-		this.roomList = [];
+	isRoomExcluded: function(roomName){
+		var state = this.config.exclude.indexOf(roomName) !== -1;
+		return state;
+	},
+	isInTVMode: function(artist, track, cover){
+                // if Sonos Playbar is in TV mode, no title is provided and therefore the room should not be displayed
+                var state = (artist && artist.length) == 0
+                	&& (track && track.length) == 0
+                        && (cover && cover.length) == 0;
+		return state;
+	},
+	updateRoomList: function(data){
+		var roomList = [];
 		$.each(data, function (i, item) {
-			var room = '';
-			var isGroup = item.members.length > 1;
-			if(isGroup){
-				$.each(item.members, function (j, member) {
-					var isExcluded = this.config.exclude.indexOf(member.roomName) !== -1;
-					room += isExcluded?'':(member.roomName + ', ');
-				}.bind(this));
-				room = room.replace(/, $/,"");
-			}else{
-				room = item.coordinator.roomName;
-				var isExcluded = this.config.exclude.indexOf(room) !== -1;
-				room = isExcluded?'':room;
-			}
-			if(room !== ''){
-				var state = item.coordinator.state.playbackState;
+			var roomName = this.getRoomName(item);
+			if(roomName !== ''){
 				var currentTrack = item.coordinator.state.currentTrack;
-				var artist = currentTrack.artist;
-				var track = currentTrack.title;
-				var cover = currentTrack.absoluteAlbumArtUri;
+				var artist = currentTrack.artist || '';
+				var track = currentTrack.title || '';
+				var cover = currentTrack.absoluteAlbumArtUri || '';
 //				var streamInfo = currentTrack.streamInfo;
 //				var type = currentTrack.type;
 
-				if(track == currentTrack.uri)
-					track = '';
+				// clean data
+                                artist = artist?artist.trim():'';
+                                track = track?track.trim():'';
+                                cover = cover?cover.trim():'';
+				track = track == currentTrack.uri?'':track;
 
-                                this.roomList.push({
-                                        'name': room,
-                                        'state': state,
-                                        'artist': artist?artist:"",
-                                        'track': track?track:"",
-                                        'albumArt': cover?cover:"",
+                                roomList.push({
+                                        'name': roomName,
+                                        'state': this.isInTVMode(artist, track, cover)?'TV':item.coordinator.state.playbackState,
+                                        'artist': artist,
+                                        'track': track,
+                                        'albumArt': cover,
                                 });
-
-			//	text += this.renderRoom(state, artist, track, cover, room);
 			}
 		}.bind(this));
 		this.loaded = true;
-		// only update dom if content changed
-		//if(this.dom !== text){
-		//	this.dom = text;
-			this.updateDom(this.config.animationSpeed);
-		//}
-	},
-	renderRoom: function(state, artist, track, cover, roomName) {
-		artist = artist?artist:"";
-		track = track?track:"";
-		cover = cover?cover:"";
-		var room = '';
-		// if Sonos Playbar is in TV mode, no title is provided and therefore the room should not be displayed
-		var isEmpty = (artist && artist.trim().length) == 0
-			&& (track && track.trim().length) == 0
-			&& (cover && cover.trim().length) == 0;
-		// show song if PLAYING
-		if(state === 'PLAYING' && !isEmpty) {
-			room += this.html.song.format(
-				// show album art if 'showAlbumArt' is set
-				(this.config.showAlbumArt && !this.config.showAlbumArtRight
-					?this.html.art.format(cover)
-					:''
-				)+
-				this.html.name.format(artist, track)+
-				// show album art if 'showAlbumArt' is set
-				(this.config.showAlbumArt && this.config.showAlbumArtRight
-					?this.html.art.format(cover)
-					:''
-				)
-				//+"<span>"+streamInfo+"</span>"
-			);
+		if (JSON.stringify(this.roomList) === JSON.stringify(roomList)){
+			return;
 		}
-		// show room name if 'showRoomName' is set and PLAYING or 'showStoppedRoom' is set
-		if(this.config.showRoomName && (state === 'PLAYING' || this.config.showStoppedRoom)) {
-			room += this.html.room.format(roomName);
-		}
-		return  this.html.roomWrapper.format(room);
+		this.roomList = roomList;
+		this.updateDom(this.config.animationSpeed);
 	},
 	getScripts: function() {
 		return [
@@ -150,8 +113,8 @@
 	},
 	socketNotificationReceived: function(notification, payload) {
 		if (notification === 'SONOS_DATA') {
-        		Log.info('received SONOS_DATA');
-			this.render(payload);
+        		Log.debug('received SONOS_DATA');
+			this.updateRoomList(payload);
       		}
   	}
 });
