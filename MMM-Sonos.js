16b522e1daaa15919e0abb22848314b494f59cc2
Christopher Fenner <CFenner@users.noreply.github.com>
2022-01-07 10:05:10 +0100

    Update MMM-Sonos.js

--- MMM-Sonos.js
+++ MMM-Sonos.js
@@ -87,13 +87,7 @@
 		}
 		this.roomList = roomList;
 		this.updateDom(this.config.animationSpeed);
-	},/*
-	getScripts: function() {
-		return [
-			'String.format.js',
-			'//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.js'
-		];
-	},*/
+	},
 	getStyles: function() {
 		return ['sonos.css'];
 	},
